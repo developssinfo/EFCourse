47215557f4122ddf140fbbbb2c50ffad5072e34a
isai villalba <iava2019@outlook.com>
2021-04-27 13:54:35 -0500

    Se agrega la primer migración al proyecto

---
+++ ConsoleApp1/Models/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    class ApplicationDbContext : DbContext
+    {
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Server=DESKTOP-3OTPLG0\SQLINSTANCE;Database=EFCourse;User Id=sa;Password=Salmo091;")
+				.EnableSensitiveDataLogging(true)
+				.UseLoggerFactory(MyLoggerFactory);
+		}
+
+		public static readonly ILoggerFactory MyLoggerFactory = LoggerFactory.Create(builder =>
+		{
+			builder
+			   .AddFilter((category, level) =>
+				   category == DbLoggerCategory.Database.Command.Name
+				   && level == LogLevel.Information)
+			   .AddConsole();
+		});
+		
+		DbSet<Estudiante> Estudiantes { get; set; }
+	}
+}
---
+++ ConsoleApp1/Models/Estudiante.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    class Estudiante
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
